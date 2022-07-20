44307b0dd0eadecbf78481fb49938dda29cf0721
AHMED <a.s1171111@gmail.com>
2022-07-20 09:36:52 +0200

    support multiLine comment

--- src/components/HTMLEngineProvider/HTMLRenderers/CommentRenderer.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/CommentRenderer.js
@@ -2,7 +2,7 @@ import _ from 'underscore';
 import React from 'react';
 import {View} from 'react-native';
 import {
-    TNodeChildrenRenderer,
+    TNodeRenderer,
 } from 'react-native-render-html';
 import htmlRendererPropTypes from './htmlRendererPropTypes';
 
@@ -10,11 +10,32 @@ const propTypes = {
     ...htmlRendererPropTypes,
 };
 
-const CommentRender = props => (
-    <View style={{alignContent: 'center', flexDirection: 'row'}}>
-        { _.map(props.tnode.children, node => (<TNodeChildrenRenderer tnode={node} />))}
-    </View>
-);
+const CommentRender = (props) => {
+    const lines = [];
+    let currentLine = [];
+    const nodes = props.tnode.children[0].children;
+    _.map(nodes, (node) => {
+        if (node.tagName !== 'br') {
+            currentLine.push(node);
+            return;
+        }
+        lines.push(currentLine);
+        currentLine = [];
+    });
+    if (currentLine.length > 0) {
+        lines.push(currentLine);
+    }
+
+    return (
+        <View>
+            {_.map(lines, line => (
+                <View style={{alignContent: 'center', flexDirection: 'row'}}>
+                    { _.map(line, node => (<TNodeRenderer tnode={node} />))}
+                </View>
+            ))}
+        </View>
+    );
+};
 
 CommentRender.propTypes = propTypes;
 CommentRender.displayName = 'CommentRenderer';
