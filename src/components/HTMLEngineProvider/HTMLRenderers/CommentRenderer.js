16ef8652363de2b7f3a4e3f000b287bad951b50c
AHMED <a.s1171111@gmail.com>
2022-07-20 18:00:50 +0200

    fix: image comment .

--- src/components/HTMLEngineProvider/HTMLRenderers/CommentRenderer.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/CommentRenderer.js
@@ -1,8 +1,9 @@
 import _ from 'underscore';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {
     TNodeRenderer,
+    TNodeChildrenRenderer,
 } from 'react-native-render-html';
 import htmlRendererPropTypes from './htmlRendererPropTypes';
 
@@ -11,6 +12,16 @@ const propTypes = {
 };
 
 const CommentRender = (props) => {
+    // render image comment.
+    if (props.tnode.children[0].tagName === 'img') {
+        return (
+            <View>
+                <TNodeChildrenRenderer tnode={props.tnode} />
+            </View>
+        );
+    }
+
+    // get lines on comment by splite <br> tag.
     const lines = [];
     let currentLine = [];
     const nodes = props.tnode.children[0].children;
@@ -26,9 +37,13 @@ const CommentRender = (props) => {
         lines.push(currentLine);
     }
 
+    // render every line in View  with flexDirection Row .
+    // and render the  comment (contain all lines)in View with flexDirection column (default style)
     return (
         <View>
             {_.map(lines, line => (
+
+                // TODO: separate style
                 <View style={{alignContent: 'center', flexDirection: 'row', flexWrap: 'wrap'}}>
                     { _.map(line, node => (
                         <TNodeRenderer tnode={node} />
